4b7994ccb856b10a1e3fe9d1286ff2da99c042bd
Daniela Codes <daniela.antonio.430@gmail.com>
2024-01-06 18:43:27 -0500

    Updated JS file
    created :
    input value
    crossout button
    
    cross out function
    delete item function
    sort
    
    ** code now working yet **

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
+// Converting Vanilla JS to jQuery 
+
 function newItem (){
-    let inputValue = $('input').val();
+    // adding new item to list 
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
     li.append(inputValue);
 
-    
+    if (inputValue === ''){
+        alert("You must write something!");
+    } else {
+        $('#list').append(li);
+    }
+ // crossing out an item for the list of items : 
+    function crossOut () {
+     li.toggleClass("strike");    
+    }
+    li.on("click", function crossOut() {
+        li.toggleClass("strike");
+    });
 
+// adding delete button "X": 
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
 
+    crossOutButton.on("click", deleteListItem);
+    function deleteListItem (){
+        li.addClass("delete")
+    }
+// Reordering the items : 
+
+    $('#list').sortable();
 
 }
 
